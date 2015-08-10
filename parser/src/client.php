10fd5db539a62fea8623beed027345dcd9199284
huyanping <hypxm@qq.com>
2015-08-10 12:04:48 +0800

    添加HTTP客户端协议解析

--- parser/src/client.php
+++ parser/src/client.php
@@ -7,7 +7,7 @@
  */
 
 $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-if(!socket_connect($socket, "www.baidu.com", 80)){
+if(socket_connect($socket, "www.baidu.com", 80) === false){
     echo socket_last_error($socket) . PHP_EOL;
     exit;
 }
@@ -25,10 +25,14 @@ Accept-Language: zh-CN,zh;q=0.8,en;q=0.6
 GLOB_MARK;
 
 
-if(!socket_write($socket, $request, strlen($request))){
+if(socket_write($socket, $request, strlen($request)) === false){
     echo socket_last_error($socket) . PHP_EOL;
     exit;
 }
 
 $response = socket_read($socket, 1024);
+if($response === false){
+    echo socket_last_error($socket);
+    exit;
+}
 echo $response . PHP_EOL;
