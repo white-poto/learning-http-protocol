ea631585092a55985a97c930fba1bb2074e1222b
huyanping <hypxm@qq.com>
2015-08-10 12:12:31 +0800

    添加HTTP客户端协议解析

--- parser/src/client.php
+++ parser/src/client.php
@@ -12,20 +12,14 @@ if(socket_connect($socket, "www.sina.com", 80) === false){
     exit;
 }
 
-$request = <<<GLOB_MARK
-GET / HTTP/1.1
-Host: www.sina.com
-Connection: keep-alive
-Cache-Control: max-age=0
-Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
-Upgrade-Insecure-Requests: 1
-User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.125 Safari/537.36
-Accept-Encoding: gzip, deflate, sdch
-Accept-Language: zh-CN,zh;q=0.8,en;q=0.6
-GLOB_MARK;
+$request_headers = array(
+    "GET / HTTP/1.1",
+    "Host: www.sina.com",
+    "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+);
 
-$request .= PHP_EOL;
-
+$request = implode("\r\n", $request_headers);
+$request .= "\r\n\r\n";
 
 if(socket_write($socket, $request, strlen($request)) === false){
     echo "ERROR:" . socket_last_error($socket) . PHP_EOL;
