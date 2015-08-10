ad8d01a5d89b5d7cdd29328597fdfa7f75d34e88
huyanping <hypxm@qq.com>
2015-08-10 12:10:13 +0800

    添加HTTP客户端协议解析

--- parser/src/client.php
+++ parser/src/client.php
@@ -22,8 +22,9 @@ Upgrade-Insecure-Requests: 1
 User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.125 Safari/537.36
 Accept-Encoding: gzip, deflate, sdch
 Accept-Language: zh-CN,zh;q=0.8,en;q=0.6
-
 GLOB_MARK;
+
+$request .= PHP_EOL;
 
 
 if(socket_write($socket, $request, strlen($request)) === false){
