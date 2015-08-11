6ba4e1855a86af64fc5ca2c8adf36bcce003e1a4
huyanping <hypxm@qq.com>
2015-08-11 09:29:24 +0800

    add keepalive

---
+++ connection/keepalive.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jenner
+ * Date: 2015/8/11
+ * Time: 9:26
+ */
+
+$keep_alive = new KeepAlive();
+$keep_alive->start();
+
+class KeepAlive {
+    public function start(){
+        $curl = curl_init("http://www.google.com");
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 60);
+        curl_setopt($curl, CURLOPT_TIMEOUT, 600);
+        $result = curl_exec($curl);
+        echo $result;
+    }
+}
