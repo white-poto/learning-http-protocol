256bcc51c61511907b8d85a89627763eac8a1bb1
huyanping <hypxm@qq.com>
2015-08-11 09:38:35 +0800

    add keepalive

--- connection/keepalive.php
+++ connection/keepalive.php
@@ -11,17 +11,20 @@ $keep_alive->start();
 
 class KeepAlive {
     public function start(){
-        $curl = curl_init("http://www.google.com");
+        $curl = curl_init("http://www.baidu.com/s?word=xcx");
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
         curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 60);
         curl_setopt($curl, CURLOPT_TIMEOUT, 600);
         curl_setopt($curl, CURLOPT_NOBODY, true);
         curl_setopt($curl, CURLOPT_HEADER, true);
-        $result = curl_exec($curl);
-        echo $result;
 
-        $result = curl_exec($curl);
-        echo $result;
+        while(true){
+            $url = "http://www.baidu.com/s?word=" . mt_rand(0, 1000);
+            curl_setopt($curl, CURLOPT_URL, $url);
+            $result = curl_exec($curl);
+            echo "get result:" . strlen($result) . PHP_EOL;
+        }
+
     }
 }
