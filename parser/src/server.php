f69b96c76d2e9f4338c6929c9e0b069fd6cacd05
huyanping <hypxm@qq.com>
2015-08-12 14:04:56 +0800

    fix bug

--- parser/src/server.php
+++ parser/src/server.php
@@ -11,6 +11,7 @@
 
 
 $server = new Server();
+//注册响应回调函数
 $server->registerHandler(function($connection, $header, $body){
     echo "get request: " . time() . PHP_EOL;
     echo "header: " . PHP_EOL . $header . PHP_EOL;
@@ -22,15 +23,30 @@ $server->registerHandler(function($connection, $header, $body){
     socket_write($connection, $response, strlen($response));
 });
 
+//启动server
 $server->start();
 
+
+/**
+ * 简易http server 支持http协议解析
+ * Class Server
+ */
 class Server
 {
 
+    /**
+     * @var string 字符流缓存
+     */
     protected $cache = "";
 
+    /**
+     * @var http请求处理器
+     */
     protected $handler;
 
+    /**
+     * 启动server
+     */
     public function start()
     {
         $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
@@ -55,11 +71,21 @@ class Server
         }
     }
 
+    /**
+     * 注册处理器
+     * @param $handler
+     */
     public function registerHandler($handler)
     {
         $this->handler = $handler;
     }
 
+    /**
+     * http协议解析
+     * @param $connection
+     * @param $data
+     * @return bool
+     */
     protected function parse($connection, $data)
     {
         $data = $this->cache . $data;
@@ -75,18 +101,17 @@ class Server
         $header = $http_info[0];
         $body = count($http_info) > 1 ? $http_info[1] : 0;
 
-        if (empty($body)) {
-            call_user_func($this->handler, $connection, $header, null);
-            socket_close($connection);
-            return true;
-        }
-
         $content_length = $this->getContentLength($header);
-        if ($content_length == 0) {
+
+
+        if ($content_length == 0) { // 正好是一个http帧
             call_user_func($this->handler, $connection, $header, null);
             socket_close($connection);
             return true;
-        } else {
+        } elseif($content_length > strlen($body)){ // 缺少body部分
+            $this->cache = $data;
+            return false;
+        } else { // 发生粘包，摘出当前包，缓存剩余部分
             $body = substr($body, 0, $content_length);
             $this->cache = substr($body, $content_length);
             call_user_func($this->handler, $connection, $header, $body);
@@ -94,6 +119,11 @@ class Server
         }
     }
 
+    /**
+     * 获取content-length
+     * @param $headers
+     * @return int
+     */
     protected function getContentLength($headers)
     {
         $headers = explode("\r\n", $headers);
